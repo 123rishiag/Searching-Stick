690860dd38967c9d845010fc4a5c8b4a46998633
Rishabh Garg <narishabh.garg@gmail.com>
2024-10-13 19:25:02 +0530

    Implemented Linear Search

--- Searching-Stick/source/Gameplay/GameplayService.cpp
+++ Searching-Stick/source/Gameplay/GameplayService.cpp
@@ -22,6 +22,16 @@ namespace Gameplay
 	void GameplayService::initialize()
 	{
 		gameplay_controller->initialize();
+		initializeRandomSeed();
+	}
+
+	void GameplayService::initializeRandomSeed() // Helper function for initializing the random seed
+	{
+		// Seed the random number generator with the current time
+		// This ensures that the sequence of random numbers will be different each time the program is run
+		// The `std::time(nullptr)` function returns the current time as the number of seconds since the Unix epoch (January 1, 1970)
+		// The `static_cast<unsigned int>` is used to cast the `std::time_t` value to an `unsigned int`, which is required by `std::srand`
+		std::srand(static_cast<unsigned int>(std::time(nullptr)));
 	}
 
 	void GameplayService::update()
--- Searching-Stick/source/Sound/SoundService.cpp
+++ Searching-Stick/source/Sound/SoundService.cpp
@@ -19,7 +19,9 @@ namespace Sound
 	void SoundService::loadSoundFromFile()
 	{
 		if (!buffer_button_click.loadFromFile(Config::button_click_sound_path))
-			printf("Error loading background music file");
+			printf("Error loading button click sound file");
+		if (!buffer_compare_sfx.loadFromFile(Config::compare_sfx_path))
+			printf("Error loading compare sfx sound file");
 	}
 
 	void SoundService::playSound(SoundType soundType)
@@ -28,6 +30,9 @@ namespace Sound
 		{
 		case SoundType::BUTTON_CLICK:
 			sound_effect.setBuffer(buffer_button_click);
+			break;
+		case SoundType::COMPARE_SFX:
+			sound_effect.setBuffer(buffer_compare_sfx);
 			break;
 		default:
 			printf("Invalid sound type");
