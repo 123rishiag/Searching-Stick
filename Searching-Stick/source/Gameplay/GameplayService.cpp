ad96e6a5acef314d3b4315c1ebf3f5438a7633b8
Rishabh Garg <narishabh.garg@gmail.com>
2024-10-13 21:14:00 +0530

    Created Gameplay UI Controller

--- Searching-Stick/source/Gameplay/GameplayService.cpp
+++ Searching-Stick/source/Gameplay/GameplayService.cpp
@@ -12,16 +12,19 @@ namespace Gameplay
 	GameplayService::GameplayService()
 	{
 		gameplay_controller = new GameplayController();
+		collection_controller = new StickCollectionContoller();
 	}
 
 	GameplayService::~GameplayService()
 	{
 		delete (gameplay_controller);
+		delete(collection_controller);
 	}
 
 	void GameplayService::initialize()
 	{
 		gameplay_controller->initialize();
+		collection_controller->initialize();
 		initializeRandomSeed();
 	}
 
@@ -37,15 +40,53 @@ namespace Gameplay
 	void GameplayService::update()
 	{
 		gameplay_controller->update();
+		collection_controller->update();
 	}
 
 	void GameplayService::render()
 	{
 		gameplay_controller->render();
+		collection_controller->render();
+	}
+
+	void GameplayService::searchElement(Collection::SearchType search_type)
+	{
+		collection_controller->searchElement(search_type);
+	}
+
+	Collection::SearchType GameplayService::getCurrentSearchType()
+	{
+		return collection_controller->getSearchType();
+	}
+
+	int GameplayService::getNumberOfComparisons()
+	{
+		return collection_controller->getNumberOfComparisons();
+	}
+
+	int GameplayService::getNumberOfArrayAccess()
+	{
+		return collection_controller->getNumberOfArrayAccess();
+	}
+
+	int GameplayService::getNumberOfSticks()
+	{
+		return collection_controller->getNumberOfSticks();
+	}
+
+	int GameplayService::getDelayMilliseconds()
+	{
+		return collection_controller->getDelayMilliseconds();
+	}
+
+	sf::String GameplayService::getTimeComplexity()
+	{
+		return collection_controller->getTimeComplexity();
 	}
 
 	void GameplayService::reset()
 	{
 		gameplay_controller->reset();
+		collection_controller->reset();
 	}
 }
